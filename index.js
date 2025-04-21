3e1ecd81cd41bc809435bfc62f1282147fbf1ab5
Yash <theflashyash99@gmail.com>
2025-04-21 14:05:06 +0530

    Cravory-01

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Header = () => { 
+    return (
+        <div className="header">
+        <div>
+            <img />
+        </div>
+
+
+
+            
+        </div>
+    )
+
+}
+
+const App = () => {
+    return(
+        <div className="app">
+        //Header
+
+ 
+        </div>
+
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
