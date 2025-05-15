bb383fe52ca7b44a460b28d53f302c653a4e5dcf
Yash <theflashyash99@gmail.com>
2025-05-15 13:42:16 +0530

    Episord 5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,37 @@
-import resList from '../../API file/restList';
-import RestaurantCard from './RestaurantCard';
+import { useState } from "react";
+import resList from "../../API file/restList";
+import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  // 1. Create a state to hold the restaurant list
+  const [restaurants, setRestaurants] = useState(resList); 
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {}}>
+        {/* 2. On button click, update the state with filtered list */}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = resList.filter((res) => res.star > 4);
+            setRestaurants(filteredList);
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
 
       <div className="res-container">
-        {resList.map((resList) => (
+        {/* 3. Render the current state (filtered or full list) */}
+        {restaurants.map((res) => (
           <RestaurantCard
-            key={resList.id}
-            name={resList.name}
-            cuisine={resList.cuisine}
-            star={resList.star}
-            time={resList.time}
-            img={resList.img}
-            alt={resList.alt}
+            key={res.id}
+            name={res.name}
+            cuisine={Array.isArray(res.cuisine) ? res.cuisine.join(", ") : res.cuisine}
+            star={res.star}
+            time={res.time}
+            img={res.img}
+            alt={res.alt}
           />
         ))}
       </div>
